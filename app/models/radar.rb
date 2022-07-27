b92bef98560c35cc1a173f10340d568f9b0dff3c
kohrVid <kohrVid@zoho.com>
2022-07-27 14:25:17 +0200

    Handle different different Attack Modes target order
    A later commit will implement the PrioritizeTX Attack Mode which changes
    the target order in the API response. As such, it should be possible to
    pass in the target order as an argument in the Radar model's
    serialisation method. Because complicated SQL statements are expected,
    the method will now make use of Arel queries to order a list of targets.

--- app/models/radar.rb
+++ app/models/radar.rb
@@ -4,11 +4,16 @@ class Radar < ApplicationRecord
   accepts_nested_attributes_for :position, allow_destroy: true
   accepts_nested_attributes_for :targets, allow_destroy: true
 
-  def as_json(options={})
+  def as_json(options = { target_order: "damage desc" })
+    sanitized_sql_statement = ActiveRecord::Base.sanitize_sql_array(
+      [options[:target_order]]
+    )
+
     {
       position: position.as_json,
       targets: targets.where.not(damage: nil)
-        .order("damage desc")&.map(&:target_type)
+                      .order(Arel.sql(sanitized_sql_statement))
+                      &.map(&:target_type)
     }
   end
 end
